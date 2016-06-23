a6da51cfeda1826684c45cb178567036a9f48e1a
Jose Arceo <arceojg@gmail.com>
2016-06-23 17:19:44 +0000

    arceojg
    ejercicios
    
    Signed-off-by: Jose Arceo <arceojg@gmail.com>

---
+++ progs/lectanalog.py
@@ -0,0 +1,11 @@
+import mraa
+import time
+try:
+	pinSensor = mraa.Aio(3)
+	pinSensor.setBit(12)
+	while True:
+		valorSensor = pinSensor.read()
+		print "%.6f" % (valorSensor/819.0)
+		time.sleep(1)
+except:
+	print "Seguro que tienes un ADC?"	
